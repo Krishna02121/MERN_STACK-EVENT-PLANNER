0015e8e71813974b67dde519ac25c9124d89406c
Krishna02121 <krishnaagrawal15881@gmail.com>
2024-09-02 01:03:24 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -16,8 +16,17 @@ app.use(
   })
 );
 
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/",(req,res)=>{
+  res.json({
+    success:true,
+    message:"JAI SHREE RAM",
+  })
+});
 
 app.use("/api/v1/message", messageRouter);
 
