1cd726de7a1bde02a0e3da9d0d2ed81125dc6d77
Krishna02121 <krishnaagrawal15881@gmail.com>
2024-09-02 21:52:02 +0530

    new commit

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "https://mern-stack-event-planner-8iq2.onrender.com/api/v1/message/send",
+        "http://localhost:4000/api/v1/message/send",
         {
           name,
           email,
